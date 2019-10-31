e3c11906d06d835f2be5cf63fe0b5f2b460e55f8
nowyTR <nowy.inheritance@gmail.com>
2019-10-31 11:01:37 +0100

    bind code chars to the array

--- src/components/gameText/gameText.component.tsx
+++ src/components/gameText/gameText.component.tsx
@@ -3,6 +3,14 @@ import { Wrapper, Container } from './gameText.styles'
 import { NON_TYPEABLES } from './config'
 import { hasClass } from './gameText.helpers'
 
+interface Code {
+  char: string
+  beforeComment: boolean
+  index: number
+  elIndex: number
+  el: HTMLElement
+}
+
 function isNonTypable(el: HTMLElement): boolean {
   return hasClass(el, NON_TYPEABLES)
 }
@@ -15,19 +23,56 @@ function GameText({ exercise }: { exercise: Exercise }): JSX.Element {
    * and wrap each character is a specific span tag
    */
   const bindCodeCharacters = (): void => {
-    const codemap = []
+    const codemap: Code[] = []
 
     if (!(gameCode.current instanceof HTMLElement)) {
       return
     }
     const contents = gameCode.current.hasChildNodes() ? gameCode.current.childNodes : []
 
-    contents.forEach((el, index) => {
+    contents.forEach((el, elIndex) => {
       console.log('asdasd')
       if (isNonTypable(el)) {
-        console.log('dank meme')
+        // Handle special case of end-of-line comment
+        const prev = contents[elIndex - 1]
+        const next = contents[elIndex + 1]
+
+        if (prev instanceof HTMLElement && next instanceof HTMLElement) {
+          // End-of-line comment is preceded by non-newline and
+          // followed by newline
+          const prevText = prev.textContent || ''
+          const nextText = next.textContent || ''
+          const isEndOfLineComment = !prevText.match(/\n\s*$/) && nextText.charAt(0) === '\n'
+          if (isEndOfLineComment) {
+            // Add the return at the end of the previous
+            // element
+            codemap.push({
+              char: '\n',
+              beforeComment: true,
+              index: prevText.search(/\s*$/),
+              elIndex: elIndex - 1,
+              el: prev
+            })
+          }
+        }
+        return
       }
+
+      const text = el.textContent || ''
+      text.split('').forEach((char, charIndex) => {
+        codemap.push({
+          char,
+          beforeComment: false,
+          index: charIndex,
+          elIndex,
+          el
+        })
+      })
     })
+
+    const iterativeFilter = function(collection, state, loopFn): void {
+      // da
+    }
 
     // Loop through contents of code, and add all non-comment
     // blocks into the codemap, keeping track of their positions
