c2616785a669de3c22b30ac932c42e1688f43000
nowyTR <nowy.inheritance@gmail.com>
2019-11-04 12:18:29 +0100

    adde code cursor class

---
+++ src/components/gameText/codeCursor.ts
@@ -0,0 +1,147 @@
+/**
+ * Represents a player or opponent's cursor
+ */
+class CodeCursor {
+  playerId
+  playerName
+  cursor
+  code
+  codeLength
+  pos
+  keystrokes
+  isMistaken
+  mistakePathLength
+  mistakes
+  mistakePositions
+  isMainPlayer
+  onCorrectKey
+  onAdvanceCursor
+  onRetreatCursor
+  onGameComplete
+
+  constructor(cfg) {
+    this.playerId = cfg.playerId
+    this.playerName = cfg.playerName
+    this.cursor = cfg.cursor
+    this.code = cfg.code
+    this.codeLength = cfg.code.length
+    this.pos = 0
+    this.keystrokes = 0
+    this.isMistaken = false
+    this.mistakePathLength = 0
+    this.mistakes = 0
+    this.mistakePositions = []
+
+    this.isMainPlayer = cfg.isMainPlayer || false
+
+    this.onCorrectKey = cfg.onCorrectKey || (() => undefined)
+    this.onAdvanceCursor = cfg.onAdvanceCursor || (() => undefined)
+    this.onRetreatCursor = cfg.onRetreatCursor || (() => undefined)
+    this.onGameComplete = cfg.onGameComplete || (() => undefined)
+
+    this.cursor.addClass(this.playerName)
+  }
+
+  private processKey = (key): void => {
+    if (this.isMistaken) {
+      this.mistakePathKey()
+    } else if (key === this.code.charAt(this.pos)) {
+      this.correctKey()
+    } else {
+      this.incorrectKey()
+    }
+  }
+
+  private advanceCursor = (): void => {
+    this.advanceCursorWithClass(this.playerName)
+  }
+
+  private advanceCursorWithClass = (curClass, trailingClass = ''): void => {
+    this.keystrokes += 1
+    this.pos += 1
+
+    this.cursor.removeClass(curClass)
+    if (this.isMainPlayer) {
+      this.cursor.removeClass('untyped')
+      this.cursor.addClass('typed')
+    }
+    this.cursor.addClass(trailingClass)
+
+    this.cursor = this.cursor.nextAll('.code-char').first()
+    this.cursor.addClass(curClass)
+
+    this.onAdvanceCursor.call(this, this)
+  }
+
+  private retreatCursor = (): void => {
+    this.retreatCursorWithClass(this.playerName)
+  }
+
+  private retreatCursorWithClass = (curClass, trailingClass = ''): void => {
+    this.keystrokes += 1
+    this.pos -= 1
+    this.mistakePathLength -= 1
+
+    this.cursor.removeClass(curClass)
+    this.cursor = this.cursor.prevAll('.code-char').first()
+
+    this.cursor.removeClass(trailingClass)
+    if (this.isMainPlayer) {
+      this.cursor.removeClass('typed')
+      this.cursor.addClass('untyped')
+    }
+    this.cursor.addClass(curClass)
+
+    this.onRetreatCursor.call(this, this)
+  }
+
+  private correctKey = (): void => {
+    this.advanceCursorWithClass(this.playerName)
+
+    this.onCorrectKey.call(this, this)
+    if (this.pos === this.codeLength) {
+      this.onGameComplete.call(this, this)
+    }
+  }
+
+  private incorrectKey = (): void => {
+    // We must *not* be at the final character of the code if we want to
+    // create a mistake path, so check for it
+    if (this.pos < this.codeLength - 1) {
+      this.isMistaken = true
+      this.mistakes += 1
+      this.mistakePositions.push(this.pos)
+      this.advanceCursorWithClass(this.playerName, 'mistake')
+      this.mistakePathLength += 1
+    }
+    // But we do want to highlight a mistake, even if we're at the end
+    // of the code
+    this.cursor.addClass('mistaken')
+  }
+
+  private mistakePathKey = (): void => {
+    if (this.pos < this.codeLength - 1) {
+      if (this.mistakePathLength < 10) {
+        this.advanceCursorWithClass(`${this.playerName} mistaken`, 'mistake-path')
+        this.mistakePathLength += 1
+      }
+    }
+  }
+
+  private backspaceKey = (): void => {
+    if (this.isMistaken) {
+      this.retreatCursorWithClass(`${this.playerName} mistaken`, 'mistake-path mistake')
+
+      if (this.mistakePathLength === 0) {
+        this.isMistaken = false
+        this.cursor.removeClass('mistaken')
+      }
+    }
+  }
+
+  public destroy = (): void => {
+    this.cursor.removeClass(this.playerName)
+  }
+}
+
+export default CodeCursor
--- src/components/gameText/gameText.component.tsx
+++ src/components/gameText/gameText.component.tsx
@@ -1,9 +1,65 @@
 import React from 'react'
 import { Wrapper, Container } from './gameText.styles'
 import UseBindCodeCharacters from './useBindCodeCharacters'
+import CodeCursor from './codeCursor'
+
+interface GameState {
+  time: any
+  startTime: any
+  code: any
+  playerCursor: any
+  opponents: any
+  opponentCursors: any
+}
+
+const gameReducer = (state, action): GameState => {
+  switch (action.type) {
+    case 'START_GAME':
+      return {
+        ...state,
+        startTime: action.payload
+      }
+    default:
+      throw new Error('Wrong action provided to the gameReducer')
+  }
+}
 
 function GameText({ exercise }: { exercise: Exercise }): JSX.Element {
   const [htmlData, gameCode] = UseBindCodeCharacters(exercise.code)
+  const [state, dispatch] = React.useReducer(gameReducer, {
+    time: null,
+    startTime: null,
+    code: null,
+    playerCursor: null,
+    opponents: 0,
+    opponentCursors: {}
+  })
+  const [started, setStarted] = React.useState(false)
+
+  React.useEffect((): void => {
+    const startGame = (): void => {
+      if (started) {
+        return
+      }
+      // state.startTime = new Date()
+      dispatch({ type: 'START_GAME', payload: new Date() })
+      setStarted(true)
+    }
+    startGame()
+
+    if (!state.playerCursor) {
+      state.playerCursor = new CodeCursor({
+        isMainPlayer: true,
+        playerId: '',
+        playerName: 'player',
+        cursor: '',
+        code: state.code,
+        onAdvanceCursor: '',
+        onRetreatCursor: '',
+        onGameComplete: ''
+      })
+    }
+  }, [])
 
   return (
     <Wrapper>
